b740a017fc0749529ed0f455d31f2ba4d8c174c6
nhj12311 <nhj12311@gmail.com>
2025-10-12 04:41:35 +0900

    add pm2

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,22 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: 'coinfo', // PM2에서 표시될 앱 이름
-      port: 3000, // 앱이 실행될 포트
-      interpreter : 'bun',
-      exec_mode: 'fork', // 포크로 실행
-      instances: '1', // CPU 코어 수를 사용하지 않음. 우선 1
-      script: '.output/server/index.mjs ', // bun으로 실행할 Nuxt 서버 시작 파일
-      args : '',
-      env: {
-        // 앱에서 process.env.NODE_ENV로 접근 가능
-        "NODE_ENV": "production",
-        // 여기에 다른 운영 환경 변수를 추가할 수 있습니다.
-        // 예: "UPBIT_API_KEY": "your_api_key"
-      },
-      // 로그 파일 경로 설정 (선택 사항)
-      // out_file: './logs/out.log',
-      // error_file: './logs/error.log',
-    },
-  ],
+    apps: [{
+        name: 'coinfo',
+        script: '.output/server/index.mjs',
+        interpreter: 'bun',
+        instances: 1,
+        exec_mode: 'fork',
+        env: {
+            NODE_ENV: 'production',
+            PORT: 7300,
+        },
+        autorestart: true,
+        max_restarts: 10,
+        min_uptime: '10s',
+        watch: false,
+    }]
 };
