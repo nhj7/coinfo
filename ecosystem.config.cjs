378f74325db774d587253b0ed3de9259adf8f024
nhj12311 <nhj12311@gmail.com>
2025-10-12 04:16:52 +0900

    add pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'coinfo', // PM2에서 표시될 앱 이름
+      port: 3000, // 앱이 실행될 포트
+      exec_mode: 'cluster', // 클러스터 모드로 실행하여 CPU 코어를 모두 활용
+      instances: '1', // CPU 코어 수만큼 인스턴스 생성
+      script: './.output/server/index.mjs', // Nuxt 앱의 시작 스크립트
+      env: {
+        // 앱에서 process.env.NODE_ENV로 접근 가능
+        "NODE_ENV": "production",
+        // 여기에 다른 운영 환경 변수를 추가할 수 있습니다.
+        // 예: "UPBIT_API_KEY": "your_api_key"
+      },
+      // 로그 파일 경로 설정 (선택 사항)
+      // out_file: './logs/out.log',
+      // error_file: './logs/error.log',
+    },
+  ],
+};
