d77b7a5cd8a67eeb076a3c4c5d1dcfe0eb1a20af
nhj12311 <nhj12311@gmail.com>
2025-10-12 04:23:22 +0900

    add pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,10 +4,9 @@ module.exports = {
       name: 'coinfo', // PM2에서 표시될 앱 이름
       port: 3000, // 앱이 실행될 포트
       interpreter: 'bun', // 인터프리터를 bun으로 설정
-      exec_mode: 'fork', // 클러스터 모드로 실행하여 CPU 코어를 모두 활용
+      exec_mode: 'cluster', // 클러스터 모드로 실행하여 CPU 코어를 모두 활용
       instances: '1', // 우선은 1
-      script: 'run', // 'bun' 뒤에 오는 명령어
-      args: './.output/server/index.mjs', // 'run' 명령어의 인자
+      script: './.output/server/index.mjs', // bun으로 실행할 Nuxt 서버 시작 파일
       env: {
         // 앱에서 process.env.NODE_ENV로 접근 가능
         "NODE_ENV": "production",
